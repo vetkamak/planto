eb0423f61f4a3c18203599f9e166a5c58c8d444f
Елизавета Макарова <vetka.mak@gmail.com>
2017-12-17 16:50:09 +0700

    added settings for running Android application

--- android/app/src/main/java/com/planto/MainActivity.java
+++ android/app/src/main/java/com/planto/MainActivity.java
@@ -1,15 +1,17 @@
 package com.planto;
 
-import com.facebook.react.ReactActivity;
+// changed ReactActivity extension to react-native-navigation one
+//import com.facebook.react.ReactActivity;
+import com.reactnativenavigation.controllers.SplashActivity;
 
-public class MainActivity extends ReactActivity {
+public class MainActivity extends SplashActivity {
 
     /**
      * Returns the name of the main component registered from JavaScript.
      * This is used to schedule rendering of the component.
      */
-    @Override
-    protected String getMainComponentName() {
-        return "Planto";
-    }
+//    @Override
+//    protected String getMainComponentName() {
+//        return "Planto";
+//    }
 }
